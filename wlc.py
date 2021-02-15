95f88fc75abc675f779626f1e393945ebcc11616
Faisal <76188653+Faisalvai@users.noreply.github.com>
2021-02-16 05:01:37 +0600

    Update wlc.py

--- wlc.py
+++ wlc.py
@@ -2,7 +2,7 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\033[1;31m","\033[1;32m"]
- = random.choice(color)+"We Are NoobZ \n"
+m = random.choice(color)+"We Are NoobZ \n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
